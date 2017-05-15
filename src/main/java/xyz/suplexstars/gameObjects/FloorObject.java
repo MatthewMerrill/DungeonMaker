8d67626db6fad055305643f9d676fc6e18061ece
brandondsherman <brandondsherman@gmail.com>
2017-05-15 06:18:25 -0700

    fixed package headers

--- src/main/java/xyz/suplexstars/gameObjects/FloorObject.java
+++ src/main/java/xyz/suplexstars/gameObjects/FloorObject.java
@@ -1,3 +1,5 @@
+package xyz.suplexstars.gameObject;
+
 public abstract class FloorObject{
 
 }
--- src/main/java/xyz/suplexstars/gameObjects/Tree.java
+++ src/main/java/xyz/suplexstars/gameObjects/Tree.java
@@ -1,4 +1,4 @@
-package xyz.suplexstars;
+package xyz.suplexstars.gameObject;
 
 /**
  * Created by merrillm on 5/12/17.
